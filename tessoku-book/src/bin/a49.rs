07c55184093fc55f7e9c3ed1ac1d9bd9a93f92fa
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2025-02-12 01:36:12 +0900

    メモリアロケーションの削減

--- tessoku-book/src/bin/a49.rs
+++ tessoku-book/src/bin/a49.rs
@@ -99,9 +99,10 @@ impl State {
 fn beam_search(input: &Input) -> State {
     let mut beam: Vec<State> = Vec::with_capacity(BEAM_WIDTH);
     beam.push(State::new());
+    let mut next_beam: Vec<State> = Vec::with_capacity(beam.len() * 2);
 
     for turn in 0..input.T {
-        let mut next_beam: Vec<State> = Vec::with_capacity(beam.len() * 2);
+        next_beam.clear();
         for state in beam.iter() {
             let p = input.P[turn];
             let q = input.Q[turn];
@@ -114,7 +115,7 @@ fn beam_search(input: &Input) -> State {
             next_beam.select_nth_unstable_by_key(BEAM_WIDTH, |s| Reverse(s.score));
             next_beam.truncate(BEAM_WIDTH);
         }
-        beam = next_beam;
+        std::mem::swap(&mut beam, &mut next_beam);
     }
     // beam 内で最大の累積得点を持つ状態を返す
     *beam.iter().max_by_key(|s| s.score).unwrap()
